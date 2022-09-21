bb9a1e44b0915e2874dd1cde129e0b3122e80b05
Ramiro Berrelleza <ramiro@okteto.com>
2022-09-20 18:26:30 -0700

    use /ht as the healthcheck

--- settings/develop.py
+++ settings/develop.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'health_check',   
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
